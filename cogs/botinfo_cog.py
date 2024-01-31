a47783d80c5509694b2048872f9971aea34961c7
bluestar <canikissfemboy@gmail.com>
2024-01-31 04:58:35 +0000

    add an commit info field

--- cogs/botinfo_cog.py
+++ cogs/botinfo_cog.py
@@ -1,15 +1,20 @@
+import os
 import platform
 import discord
+import git
 from utils.createEmbed import create_embed
 from discord.commands import slash_command
 from discord.ext import commands
 
+
+repo = git.Repo(os.getcwd())
 
 
 
 class BotInfoCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.latest_commit = repo.head.commit
 
     @slash_command()
     async def botinfo(self, ctx):
@@ -19,7 +24,8 @@ class BotInfoCog(commands.Cog):
         color=discord.Color.purple(),
         fields=[
             {'name': 'Python Version', 'value': platform.python_version()},
-            {'name': 'Pycord Version', 'value': discord.__version__}
+            {'name': 'Pycord Version', 'value': discord.__version__},
+            {'name': 'Git commit', 'value': f"Commit hash: {self.latest_commit.hexsha}\nAuthor: {self.latest_commit.author.name}\nCommit Date: {self.latest_commit.authored_datetime}\nCommit msg: {self.latest_commit.message}"}
         ],
     )
 
